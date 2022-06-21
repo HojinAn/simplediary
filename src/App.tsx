fd6f31e54c81fbe9c164d98a5a012977e7913aac
안호진 <asdv94@naver.com>
2022-06-21 20:16:11 +0900

    fetch를 통해 dummy data 얻어오기 from jsonplaceholder fetch해온 data를 any로 밖에 못해주는 것인가?

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
 
 export interface DiaryInfo {
   id: number;
@@ -12,9 +11,32 @@ export interface DiaryInfo {
   created_date: number;
 }
 
+// https://jsonplaceholder.typicode.com/comments
+
 function App() {
   const [data, setData] = useState<Array<DiaryInfo>>([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it: any) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author: string, content: string, emotion: number) => {
     const created_date = new Date().getTime();
@@ -45,7 +67,6 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onRemove={onRemove} onEdit={onEdit} diaryList={data} />
     </div>
