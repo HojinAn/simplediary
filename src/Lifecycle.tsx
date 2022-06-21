940e072d6e2d013d1c9618773ced72b82ed294f4
안호진 <asdv94@naver.com>
2022-06-21 16:49:44 +0900

    short-circuit을 이용한 mount, unmount 실습

--- src/Lifecycle.tsx
+++ src/Lifecycle.tsx
@@ -1,41 +1,24 @@
 import React, { useEffect, useState } from "react";
 
-const Lifecycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
+const UnmountTest = () => {
   useEffect(() => {
     console.log("Mount!");
-  }, []);
-  useEffect(() => {
-    console.log("Update!");
-  });
-  useEffect(() => {
-    console.log(`count is updated: ${count}`);
-    if (count > 5) {
-      alert("count가 5를 넘었습니다. 1로 초기화힙니다.");
-      setCount(1);
-    }
-  }, [count]);
-  useEffect(() => {
-    console.log(`text is updated: ${text}`);
-  }, [text]);
 
+    return () => {
+      // Unmount 시점에 실행되게 됨
+      console.log("Unmount!");
+    };
+  }, []);
+
+  return <div>Unmount Testing Component</div>;
+};
+const Lifecycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
   return (
     <div style={{ padding: 20 }}>
-      <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <button onClick={() => setCount(count - 1)}>-</button>
-      </div>
-      <div>
-        <input
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest />}
     </div>
   );
 };
