3c59dca397d9afc4447c0a4cafd314572834d706
안호진 <asdv94@naver.com>
2022-06-21 15:10:55 +0900

    onEdit props 전달을 통한 item 수정, if 문 by short-circuit

--- src/DiaryItem.tsx
+++ src/DiaryItem.tsx
@@ -1,6 +1,8 @@
 import { DiaryInfo } from "./App";
+import { useRef, useState } from "react";
 interface ItemProps extends DiaryInfo {
   onRemove: Function;
+  onEdit: Function;
 }
 const DiaryItem = ({
   author,
@@ -9,7 +11,32 @@ const DiaryItem = ({
   emotion,
   id,
   onRemove,
+  onEdit,
 }: ItemProps) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const [localContent, setLocalContent] = useState(content);
+  const localContentInput = useRef<HTMLTextAreaElement>(null);
+
+  const handleRemove = () => {
+    window.confirm(`${id}번재 일기를 정말 삭제하시겠습니까?`) && onRemove(id);
+  };
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current?.focus();
+      return;
+    }
+    window.confirm(`${id}번재 일기를 정말 수정하시겠습니까?`) &&
+      (onEdit(id, localContent) || setIsEdit(false));
+  };
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -19,15 +46,32 @@ const DiaryItem = ({
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className="content">{content}</div>
-      <button
-        onClick={() => {
-          window.confirm(`${id}번재 일기를 정말 삭제하시겠습니까?`) &&
-            onRemove(id);
-        }}
-      >
-        삭제하기
-      </button>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => {
+                setLocalContent(e.target.value);
+              }}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+      {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>수정취소</button>
+          <button onClick={handleEdit}>수정완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
     </div>
   );
 };
--- src/DiaryList.tsx
+++ src/DiaryList.tsx
@@ -4,16 +4,17 @@ import DiaryItem from "./DiaryItem";
 interface ListProps {
   diaryList: Array<DiaryInfo>;
   onRemove: Function;
+  onEdit: Function;
 }
 
-const DiaryList = ({ diaryList, onRemove }: ListProps) => {
+const DiaryList = ({ diaryList, onRemove, onEdit }: ListProps) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h2>{diaryList.length}개의 일기가 있습니다.</h2>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={it.id} onRemove={onRemove} {...it} />
+          <DiaryItem key={it.id} onRemove={onRemove} onEdit={onEdit} {...it} />
         ))}
       </div>
     </div>
